0b9c69a8c384889072846d0c7d09d321d752104f
liufei <feiliu@58coin.com>
2019-07-24 16:53:48 +0800

    middleware router

--- middleware/router.js
+++ middleware/router.js
@@ -1,6 +1,14 @@
 export default async function ({
+  route,
   redirect
 }) {
   let id = 1001
-  return redirect(`/trade/${id}`)
+  if (route.name === 'index') {
+    return redirect(`/trade/${id}`)
+  }
+  if (process.client) {
+    document.documentElement.scrollTop = 0;
+    window.pageYOffset = 0;
+    document.body.scrollTop = 0;
+  }
 }
